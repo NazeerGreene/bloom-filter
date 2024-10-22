51c55bcd3133dad8ad676940b5a450570ae314f4
Nazeer Greene <nazeerg5@gmail.com>
2024-10-21 21:56:56 -0700

    helper functions to easily calculate memory requirements + optimal hash functions

--- src/main/java/learn/App.java
+++ src/main/java/learn/App.java
@@ -2,6 +2,15 @@ package learn;
 
 public class App {
     public static void main(String[] args) {
+        long nElementsInserted = 1000;
+        double DSF = 0.01; // desired false positivity probability
+        double bPerB = 8.0; // bits per Mb
 
+        long sBitArray = Helpers.calculateBitArraySize(DSF, nElementsInserted);
+        System.out.println("The bit array size for " + nElementsInserted + " at " + DSF + ": " + sBitArray + " bits.");
+        System.out.println("Or... " + (long) Math.ceil(sBitArray/bPerB) + " bytes of memory");
+
+        long nHashFunctions = Helpers.calculateNumOfHashFunctions(sBitArray, nElementsInserted);
+        System.out.println("The optimal hash functions for " + nHashFunctions + " with a bit array size of " + sBitArray + ": " + nHashFunctions + " hashes.");
     }
 }
